f7a98e15c2f6cb7da441d60f7ecde4031c0152d1
daniellehonoria <daniellehonoria@gmail.com>
2022-09-13 18:05:19 -0300

    Resolução exercícios da aula de laços 2

---
+++ exercicio2/script.js
@@ -0,0 +1,24 @@
+/*Crie um programa que peça um input de número para o usuário. Com este número, o código deve imprimir a **tabuada** do número, de 1 a 10.
+
+**Utilize o for...in para resolver**
+
+Exemplo com entrada **`7`**:
+```jsx
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+```*/
+
+const num = Number(prompt("Digite um número"))
+const tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let indice in tabuada){
+    indice ++
+    console.log(`${num} x ${num} x ${indice} = ${num * indice}`)
+}
